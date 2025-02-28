9ee35b4e96c50fb25364f92a149ad280af107ce0
autoantwort <Leander.schulten@rwth-aachen.de>
2025-02-28 20:52:26 +0100

    Remove duplicated implementation

--- src/vcpkg/base/files.cpp
+++ src/vcpkg/base/files.cpp
@@ -3843,28 +3843,6 @@ namespace vcpkg
 #endif
         }
 
-        virtual int64_t last_write_time(const Path& target, std::error_code& ec) const override
-        {
-#if defined(_WIN32)
-            auto result = stdfs::last_write_time(to_stdfs_path(target), ec);
-            return result.time_since_epoch().count();
-#else // ^^^ _WIN32 // !_WIN32 vvv
-            struct stat s;
-            if (::lstat(target.c_str(), &s) == 0)
-            {
-                ec.clear();
-#ifdef __APPLE__
-                return s.st_mtimespec.tv_sec * 1'000'000'000 + s.st_mtimespec.tv_nsec;
-#else
-                return s.st_mtim.tv_sec * 1'000'000'000 + s.st_mtim.tv_nsec;
-#endif
-            }
-
-            ec.assign(errno, std::generic_category());
-            return {};
-#endif // ^^^ !_WIN32
-        }
-
 #if defined(_WIN32)
         // FILETIME contains a 64-bit value representing the number of 100-nanosecond intervals since January 1, 1601
         // (UTC). shift epoch by 400 years to fit into int64_t (can hold 292 years)
